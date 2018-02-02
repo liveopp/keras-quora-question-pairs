60e96f8620ed6a50c76232a7f7a135e247f3123b
张航 <zhanghanghank@didichuxing.com>
2018-02-02 09:39:42 +0800

    add bidirection

--- keras-quora-question-pairs.py
+++ keras-quora-question-pairs.py
@@ -6,7 +6,8 @@ from os.path import expanduser, exists
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Model
-from keras.layers import Input, LSTM, TimeDistributed, Dense, Lambda, concatenate, Dropout, BatchNormalization
+from keras.layers import Input, LSTM, TimeDistributed, Dense,\
+    Bidirectional, Lambda, Multiply, Subtract, concatenate, Dropout, BatchNormalization
 from keras.layers.embeddings import Embedding
 from keras.regularizers import l2
 from keras.callbacks import Callback, ModelCheckpoint
@@ -137,7 +138,7 @@ q1 = Embedding(nb_words + 1,
                  trainable=False)(question1)
 #q1 = TimeDistributed(Dense(EMBEDDING_DIM, activation='relu'))(q1)
 #q1 = Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM, ))(q1)
-q1 = LSTM(128)(q1)
+q1 = Bidirectional(LSTM(128))(q1)
 
 q2 = Embedding(nb_words + 1, 
                  EMBEDDING_DIM, 
@@ -146,9 +147,12 @@ q2 = Embedding(nb_words + 1,
                  trainable=False)(question2)
 #q2 = TimeDistributed(Dense(EMBEDDING_DIM, activation='relu'))(q2)
 #q2 = Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM, ))(q2)
-q2 = LSTM(128)(q2)
+q2 = Bidirectional(LSTM(128))(q2)
 
-merged = concatenate([q1,q2])
+distance = K.abs(Subtract()([q1, q2]))
+angle = Multiply()([q1, q2])
+
+merged = concatenate([distance, angle])
 merged = Dense(200, activation='relu')(merged)
 merged = Dropout(DROPOUT)(merged)
 merged = BatchNormalization()(merged)
